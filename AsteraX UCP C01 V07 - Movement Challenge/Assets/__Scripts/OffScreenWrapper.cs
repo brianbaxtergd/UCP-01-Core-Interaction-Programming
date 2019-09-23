fda792f7bc6860782844eb15634aaa92bbe4f7cb
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-23 14:03:19 -0500

    Solution 2C - Screen Wrap.

---
+++ AsteraX UCP C01 V07 - Movement Challenge/Assets/__Scripts/OffScreenWrapper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[RequireComponent(typeof(BoxCollider))]
+public class OffScreenWrapper : MonoBehaviour
+{
+    void Update()
+    {
+        // Adding an Update() method shows the "enabled" checkbox in the inspector.
+    }
+
+    // This is called whenever this GameObject exits the bound of OnScreenBounds.
+    private void OnTriggerExit(Collider other)
+    {
+        if (!enabled)
+            return;
+
+        // Ensure the other is OnScreenBounds.
+        ScreenBounds bounds = other.GetComponent<ScreenBounds>();
+        if (bounds == null)
+            return;
+
+        ScreenWrap(bounds);
+    }
+
+    private void ScreenWrap(ScreenBounds bounds)
+    {
+        // Wrap whichever direction is necessary.
+        Vector3 relativeLoc = bounds.transform.InverseTransformPoint(transform.position);
+        // relativeLoc is in the local coords of OnScreenBounds, 0.5f is the screen edge.
+        if (Mathf.Abs(relativeLoc.x) > 0.5f)
+        {
+            relativeLoc.x *= -1;
+        }
+        if (Mathf.Abs(relativeLoc.y) > 0.5f)
+        {
+            relativeLoc.y *= -1;
+        }
+        transform.position = bounds.transform.TransformPoint(relativeLoc);
+    }
+}
---
+++ AsteraX UCP C01 V07 - Movement Challenge/Assets/__Scripts/Vector3Extensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public static class Vector3Extensions
+{
+    static public Vector3 ComponentDivide(this Vector3 v0, Vector3 v1)
+    {
+        Vector3 vRes = v0;
+
+        // Avoid divide by zero errors.
+        if (v1.x != 0)
+        {
+            vRes.x = v0.x / v1.x;
+        }
+        if (v1.y != 0)
+        {
+            vRes.y = v0.y / v1.y;
+        }
+        if (v1.z != 0)
+        {
+            vRes.z = v0.z / v1.z;
+        }
+
+        return vRes;
+    }
+}
