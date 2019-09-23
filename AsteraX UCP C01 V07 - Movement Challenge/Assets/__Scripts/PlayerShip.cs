f04c9aac804a722ff0cadf52d0339c213074524e
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-23 12:28:07 -0500

    Solution 2A - The PlayerShip

---
+++ AsteraX UCP C01 V07 - Movement Challenge/Assets/__Scripts/PlayerShip.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerShip : MonoBehaviour
+{
+    static private PlayerShip _S;
+    static public PlayerShip S
+    {
+        get
+        {
+            return _S;
+        }
+        private set
+        {
+            if (_S != null)
+            {
+                Debug.LogWarning("Second attempt to set PlayerShip singleton _S.");
+            }
+            _S = value;
+        }
+    }
+
+    [Header("Set in Inspector")]
+    public float shipSpeed = 10f;
+    public GameObject bulletPrefab;
+
+    Rigidbody rigid;
+
+
+    private void Start()
+    {
+        S = this;
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float aX = CrossPlatformInputManager.GetAxis("Horizontal");
+        float aY = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 vel = new Vector3(aX, aY);
+        if (vel.magnitude > 1)
+        {
+            vel.Normalize();
+        }
+
+        rigid.velocity = vel * shipSpeed;
+
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+
+    }
+
+    static public float MAX_SPEED
+    {
+        get
+        {
+            return _S.shipSpeed;
+        }
+        set
+        {
+            // NOTE: This is a bad estimation of the purpose for a MAX_SPEED property.
+            _S.shipSpeed = value;
+        }
+    }
+}
---
+++ AsteraX UCP C01 V07 - Movement Challenge/Assets/__Scripts/TiltWithVelocity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltWithVelocity : MonoBehaviour
+{
+    [Tooltip("The number of degrees the ship will tilt at its maximum speed.")]
+    public int degrees = 30;
+    public bool tiltTowards = true;
+
+    private int prevDegrees = int.MaxValue;
+    private float tan;
+
+    Rigidbody rigid;
+
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (degrees != prevDegrees)
+        {
+            prevDegrees = degrees;
+            tan = Mathf.Tan(Mathf.Deg2Rad * degrees);
+        }
+        Vector3 pitchDir = (tiltTowards) ? -rigid.velocity : rigid.velocity;
+        pitchDir += Vector3.forward / tan * PlayerShip.MAX_SPEED;
+        transform.LookAt(transform.position + pitchDir);
+    }
+}
